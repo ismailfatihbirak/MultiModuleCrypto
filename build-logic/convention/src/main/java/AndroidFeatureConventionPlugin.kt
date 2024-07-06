8c756b5740da79fe5eb0f3e4b2c25e99871583a9
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-07-06 17:50:07 +0300

    convention plugins

---
+++ build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,28 @@
+import com.example.multimodulecrypto.build_logic.convention.implementation
+import com.example.multimodulecrypto.build_logic.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("multimodulecrypto.android.library")
+                apply("multimodulecrypto.android.hilt")
+                apply("org.jetbrains.kotlin.plugin.serialization")
+            }
+
+            dependencies {
+                implementation(project(":core:model"))
+                implementation(project(":core:domain"))
+                implementation(project(":core:common"))
+
+                // Define common dependencies for feature modules
+                implementation(libs.findLibrary("androidx-navigation-compose").get())
+                implementation(libs.findLibrary("kotlinx-serialization-json").get())
+                implementation(libs.findLibrary("hilt_navigation_compose").get())
+            }
+        }
+    }
+}
